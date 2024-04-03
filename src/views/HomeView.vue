0f03039f99101b9205ca1bfcdcbb8d9b502f1205
digiot <digiot.abc@gmail.com>
2024-04-03 12:11:02 +0900

    Vue 導入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,8 @@
 <template>
-  <hello-world />
+  <v-container>
+    メンテナンス中です。
+  </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
+<script lang="ts" setup>
 </script>
