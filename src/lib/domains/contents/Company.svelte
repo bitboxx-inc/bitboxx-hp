cf687b4c9f1405041b31498ed1dff9ed2c449a6c
kakerunnable <kakeru.kageshima@bootware.jp>
2024-06-05 07:24:08 +0900

    全体修正

--- src/lib/domains/contents/Company.svelte
+++ src/lib/domains/contents/Company.svelte
@@ -6,23 +6,28 @@
     let info = [
         {
             title: '社名',
-            description: '株式会社bitboxx'
+            description: '株式会社bitboxx',
+            letterSp: 16 * 5,
         },
         {
             title: '設立',
-            description: '2024年04月25日'
+            description: '2024年04月25日',
+            letterSp: 16 * 5,
         },
         {
             title: '代表者',
-            description: '代表取締役CEO　影島 翔'
+            description: '代表取締役CEO　影島 翔',
+            letterSp: 16 * 2,
         },
         {
             title: '所在地',
-            description: '103-0015<br/>東京都中央区日本橋箱崎町16-11<br/>ルミネ日本橋601'
+            description: '103-0015<br/>東京都中央区日本橋箱崎町16-11<br/>ルミネ日本橋601',
+            letterSp: 16 * 2,
         },
         {
             title: '主な業務',
-            description: '<ul><li>・自社サービス開発</li><li>・システム開発、ウェブサイト制作/運用</li><li>・デザイン</li></ul>'
+            description: '<ul><li>・自社サービス開発</li><li>・システム開発、ウェブサイト制作/運用</li><li>・デザイン</li></ul>',
+            letterSp: 16 * 1,
         },
     ]
 </script>
@@ -38,8 +43,8 @@
                 <tbody>
                 {#each info as inf}
                     <tr class="border-b-2 border-black">
-                        <th class="w-96 h-24" style="mergin-left: -24px;">
-                            <span style="font-size: 16px; letter-spacing: 24px;">{@html inf.title}</span>
+                        <th class="w-96 h-24">
+                            <span style={`letter-spacing: ${inf.letterSp}px; margin-right: -${inf.letterSp}px;`}>{@html inf.title}</span>
                         </th>
                         <td>
                             <div class="w-full p-4" style="font-size: 16px;">{@html inf.description}</div>
@@ -60,8 +65,8 @@
                 <tbody>
                 {#each info as inf}
                     <tr class="border-b-2 border-black">
-                        <th class="w-96 h-24" style="mergin-left: -24px;">
-                            <span style="font-size: 16px; letter-spacing: 24px;">{@html inf.title}</span>
+                        <th class="w-96 h-24">
+                            <span style={`letter-spacing: ${inf.letterSp}px; margin-right: -${inf.letterSp}px;`}>{@html inf.title}</span>
                         </th>
                         <td>
                             <div class="w-full p-4" style="font-size: 16px;">{@html inf.description}</div>
@@ -82,11 +87,11 @@
                 <tbody>
                 {#each info as inf}
                     <tr class="border-b-2 border-black">
-                        <th class="h-24" style="mergin-left: -24px;">
-                            <span style="font-size: 16px; letter-spacing: 24px;">{@html inf.title}</span>
+                        <th class="w-40 h-12">
+                            <span style={`font-size: 12px; letter-spacing: ${inf.letterSp / 16 * 12}px; margin-right: -${inf.letterSp / 16 * 12}px;`}>{@html inf.title}</span>
                         </th>
                         <td>
-                            <div class="w-full p-4" style="font-size: 16px;">{@html inf.description}</div>
+                            <div class="w-full p-2" style="font-size: 14px;">{@html inf.description}</div>
                         </td>
                     </tr>
                 {/each}
