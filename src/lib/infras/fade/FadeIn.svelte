61ed6438e948914e825fc644738bb2394c9f407c
kakerunnable <kakeru.kageshima@bootware.jp>
2024-06-05 11:28:34 +0900

    全体修正

--- src/lib/infras/fade/FadeIn.svelte
+++ src/lib/infras/fade/FadeIn.svelte
@@ -5,17 +5,17 @@
 <style>
 
     .fadein {
-        animation: fadein 6s ease-out forwards;
+        animation: fadein 2s ease-out forwards;
     }
 
     @keyframes fadein {
         0% {
             opacity: 0;
-            transform: translateX(0);
+            transform: translateX(-40px);
         }
         100% {
             opacity: 1;
-            transform: translateX(20px);
+            transform: translateX(0px);
         }
     }
 </style>
